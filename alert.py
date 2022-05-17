cfbba38d0e385c5f3f03d8dac9c28f8db1d87f85
Julia Hohenadel <35547679+Gerudhoh@users.noreply.github.com>
2022-05-17 10:35:00 -0400

    Update alert.py

--- alert.py
+++ alert.py
@@ -22,6 +22,7 @@ def get_weather():
 
 def will_rain_tonight():
     weather = get_weather()
+    print(weather)
     main_weather = weather['current']['weather'][0]['main']
     if main_weather.casefold() == "rain":
         return True
